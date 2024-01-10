29b8f494a26420cb124580b10e7e3d293df0dd10
Alberto Paim <albertoopaim@gmail.com>
2024-01-10 14:31:56 -0300

    teste commit

--- src/controllers/groups.ts
+++ src/controllers/groups.ts
@@ -28,7 +28,7 @@ export const getGroup: RequestHandler = async (req, res) => {
 		});
 
 		if (!foundGroup) {
-			return res.status(404).json({ message: 'Grupo não encontrado' });
+			return res.status(404).json({ message: 'Grupo não pode ser deletado' });
 		}
 
 		return res.status(200).json(foundGroup);
@@ -64,47 +64,4 @@ export const creategroup: RequestHandler = async (req, res) => {
 	}
 };
 
-export const updateGroup: RequestHandler = async (req, res) =>{
-	const {id, id_event} = req.params;
-	try {
-		const updateGroupSchema = z.object({
-			name: z.string().optional()
-		});
-
-		const body = updateGroupSchema.safeParse(req.body);
-
-		
-		if(!body.success){
-			return res.status(400).json({ message: 'Dados inválidos' });
-		}			
-		
-		const updatedGroup = await groups.updateGroupService({
-			id: parseInt(id),
-			id_event: parseInt(id_event),
-		}, body.data);
-
-		console.log(updatedGroup);
-		
-		return res.status(200).json(updatedGroup);
-
-	} catch (error) {
-		return res.status(500).json({ error });
-	}
-};
-
-export const deleteGroup : RequestHandler =  async (req, res) =>{
-	const {id_event, id} = req.params;
-	try {
-		const deletedGroup = await groups.deleteGroupService({
-			id_event: Number(id_event),
-			id: Number(id)
-		});
-
-		if(!deletedGroup){
-			return res.status(400).json({message: 'Não foi possivel deletar o grupo'});
-		}
-		return res.status(200).json(deletedGroup);
-	} catch (error) {
-		return res.status(500).json({ error });
-	}
-};
+const data = 'data asdasd asdas';
