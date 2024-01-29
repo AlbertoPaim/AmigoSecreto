80edb771229cf91ca921bf102598be6b6c3eb210
Alberto Paim <albertoopaim@gmail.com>
2024-01-29 06:28:04 -0300

    fixing server

--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,19 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import http from 'http';
-import https from 'https';
 
 import adminRoutes from './routes/admin';
 import siteRoutes from './routes/site';
-import fs from 'fs';
-import { requestInterceptor } from './utils/requestIntercepter';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-
-app.all('*', requestInterceptor);
 
 app.use('/admin', adminRoutes);
 app.use('/', siteRoutes);
 
-const runServer = (port: number, server: http.Server) => {
-	server.listen(port, () => {
-		console.log(`Running at port ${port}`);
-	});
-};
+const port = process.env.PORT || 3000;
 
-const regularServer = http.createServer(app);
+app.listen(port);
 
-if (process.env.NODE_ENV === 'production') {
-
-	const options = {
-		key: fs.readFileSync(process.env.SSL_KEY as string),
-		cert: fs.readFileSync(process.env.SSL_CERT as string)
-	};
-
-	const secServer = https.createServer(options, app);
-	runServer(80, regularServer);
-	runServer(443, secServer);
-} else {
-	const serverPort: number = process.env.PORT ? parseInt(process.env.PORT) : 9000;
-	runServer(serverPort, regularServer);
-}
